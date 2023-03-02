93f0485353e58b1be3b863d05ba25b22f2c02ad7
Илья Баранов <barilya@yandex.ru>
2023-03-02 15:48:40 +0500

    Homework 2 continuation

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,9 +1,15 @@
+from timeit import default_timer
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse('Hello World')
+    # print(request.path)
+    # print(request.method)
+    # print(request.headers)
+    # return HttpResponse('Hello World')
+    context = {
+        'time_running': default_timer(),
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
