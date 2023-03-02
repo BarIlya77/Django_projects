e25bd313a0bd19cb6f4eab39fcba267d7483268d
Илья Баранов <barilya@yandex.ru>
2023-03-02 19:48:09 +0500

    Homework completed

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -9,7 +9,13 @@ def shop_index(request: HttpRequest):
     # print(request.method)
     # print(request.headers)
     # return HttpResponse('Hello World')
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphon', 999),
+    ]
     context = {
         'time_running': default_timer(),
+        'products': products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
