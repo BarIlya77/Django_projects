de6dfbfa476b7cf8e44c8e04c8a835cbd88d1844
Илья Баранов <barilya@yandex.ru>
2023-03-03 16:11:57 +0500

    Homework 3 started

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,14 +1,11 @@
 from timeit import default_timer
 
+from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
-    # print(request.path)
-    # print(request.method)
-    # print(request.headers)
-    # return HttpResponse('Hello World')
     products = [
         ('Laptop', 1999),
         ('Desktop', 2999),
@@ -19,3 +16,15 @@ def shop_index(request: HttpRequest):
         'products': products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+    # print(request.path)
+    # print(request.method)
+    # print(request.headers)
+    # return HttpResponse('Hello World')
